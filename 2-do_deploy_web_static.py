e6f43d4c65a9986d27adcb864fbdca5818c0647f
Kalunnenna1 <nnennakalualie.com>
2022-10-31 16:28:23 -0700

    2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,30 +1,40 @@
 #!/usr/bin/python3
-"""
-Fabric script based on the file 1-pack_web_static.py that distributes an
-archive to the web servers
-"""
+""" distributes an archive to web servers """
+from os import path
+from fabric.api import put, run, cd, env
 
-from fabric.api import put, run, env
-from os.path import exists
-env.hosts = ['142.44.167.228', '144.217.246.195']
+env.hosts = ['34.204.186.164', '54.80.223.108']
+env.user = 'ubuntu'
 
 
 def do_deploy(archive_path):
-    """distributes an archive to the web servers"""
-    if exists(archive_path) is False:
+    """ returns True if all operations are successful
+        or False if file path doesn't exist
+    """
+    if not path.exists(archive_path):
         return False
-    try:
-        file_n = archive_path.split("/")[-1]
-        no_ext = file_n.split(".")[0]
-        path = "/data/web_static/releases/"
-        put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(path, no_ext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
-        run('rm /tmp/{}'.format(file_n))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
-        run('rm -rf {}{}/web_static'.format(path, no_ext))
-        run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
-        return True
-    except:
-        return False
+
+    archive_name = archive_path[9:]
+
+    remote_dir = '/data/web_static/releases/' + archive_name[:-4]
+
+    put(archive_path, '/tmp')
+
+    run('sudo mkdir -p {}'.format(remote_dir))
+
+    with cd(remote_dir):
+        run('sudo tar -xzf {}'.format('/tmp/' + archive_name))
+
+    run('sudo rm /tmp/{}'.format(archive_name))
+
+    run('sudo mv {}/web_static/* {}'.format(remote_dir, remote_dir))
+
+    run('sudo rm -rf {}/web_static'.format(remote_dir))
+
+    run('sudo rm /data/web_static/current')
+
+    run('sudo ln -s {} /data/web_static/current'.format(remote_dir))
+
+    print("New version deployed!")
+
+    return True
